3c424d65b1e427aff9ede35003a23b6bccbc2067
JAIDER GUSTAVO PARRA DAZA <jaider03parra@gmail.com>
2025-11-01 11:54:39 -0500

    Revert "cambios de url openapi.js"
    This reverts commit ad8da22a4073ee80dd939384e838213fac93b87a.

--- openapi.js
+++ openapi.js
@@ -7,7 +7,7 @@ export const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: { title: 'API NoSQL (HunterXHunter)', version: '1.0.0' },
-    servers: [{ url: process.env.BASE_URL || 'https://hunterxhunter-backendsql-production.up.railway.app' }]
+    servers: [{ url: process.env.BASE_URL || 'http://localhost:10001' }]
   },
   apis: [path.join(__dirname, './routes/*.js'), path.join(__dirname, './models/*.js')]
 };
