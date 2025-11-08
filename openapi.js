a37e6058781e752b9e1eb2fad6ab1307568d4ad6
JAIDER GUSTAVO PARRA DAZA <jaider03parra@gmail.com>
2025-11-08 09:53:06 -0500

    actualizacion openapi.js

--- openapi.js
+++ openapi.js
@@ -6,8 +6,17 @@ const __dirname = path.dirname(__filename);
 export const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
-    info: { title: 'API NoSQL (HunterXHunter)', version: '1.0.0' },
-    servers: [{ url: process.env.BASE_URL || 'http://localhost:10001' }]
+    info: {
+      title: 'API NoSQL (Hunter X Hunter)',
+      version: '1.0.0',
+      description: 'Documentación de la API de personajes (NoSQL)',
+    },
+    servers: [
+      {
+        url: process.env.BASE_URL || 'https://hunterxhunter-backendnosql.onrender.com',
+      },
+    ],
   },
-  apis: [path.join(__dirname, './routes/*.js'), path.join(__dirname, './models/*.js')]
+ 
+  apis: [path.resolve(__dirname, 'routes/*.js')],
 };
