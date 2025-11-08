ecfc6bebc833cbf647caa2af1a097466dba343b4
JAIDER GUSTAVO PARRA DAZA <jaider03parra@gmail.com>
2025-11-08 09:40:58 -0500

    actualizacion en cors

--- server.js
+++ server.js
@@ -10,12 +10,8 @@ import { swaggerOptions } from './openapi.js';
 dotenv.config();
 const app = express();
 
-// ✅ CORS explícito (antes de todo)
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
-}));
+// cors
+app.use(cors());
 
 app.use(express.json());
 
